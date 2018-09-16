bbddbd4da447437b65a6426da3155563603ace89
Milan Roberson <finagle29@gmail.com>
2018-09-16 04:13:14 -0400

    whoops squash this w/ previous

---
+++ apu.c
@@ -0,0 +1,7 @@
+#include "apu.h"
+
+Gb_Apu* apu = NULL;
+
+void init_apu() {
+        apu = new_Gb_Apu();
+}
---
+++ apu.h
@@ -0,0 +1,9 @@
+#ifndef APU_H
+#define APU_H
+
+#include <stdbool.h>
+#include "gb_apu/GBAPU_Wrapper.h"
+extern Gb_Apu *apu;
+
+void init_apu(void);
+#endif
